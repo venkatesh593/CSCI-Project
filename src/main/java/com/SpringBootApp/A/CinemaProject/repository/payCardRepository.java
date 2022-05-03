814c702e11dbdfe73cfab091d22d0fc8ef2ebd04
ShaunSnyder <sls52917@uga.edu>
2022-05-03 18:27:20 -0400

    ShowEndTimes

--- src/main/java/com/SpringBootApp/A/CinemaProject/repository/payCardRepository.java
+++ src/main/java/com/SpringBootApp/A/CinemaProject/repository/payCardRepository.java
@@ -4,7 +4,10 @@ import com.SpringBootApp.A.CinemaProject.entity.payCardEntity;
 import com.SpringBootApp.A.CinemaProject.entity.userEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface payCardRepository extends CrudRepository<payCardEntity, Long> {
     payCardEntity findById(long id);
     payCardEntity findByUser(userEntity user);
+    Collection<payCardEntity> findAllByUser(userEntity user);
 }
